d913e299a9884d19b1c90dc1d3f1e845295f9a1c
Adam M. Jacobs <adam.m.jcbs@gmail.com>
2020-05-26 21:17:40 -0400

    always show context (user@host)

--- .p10k.zsh
+++ .p10k.zsh
@@ -37,6 +37,7 @@
     vcs                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
+    context                 # user@host
     prompt_char             # prompt symbol
   )
 
@@ -841,7 +842,7 @@
 
   # Don't show context unless running with privileges or in SSH.
   # Tip: Remove the next line to always show context.
-  typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_{CONTENT,VISUAL_IDENTIFIER}_EXPANSION=
+  #typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_{CONTENT,VISUAL_IDENTIFIER}_EXPANSION=
 
   # Custom icon.
   # typeset -g POWERLEVEL9K_CONTEXT_VISUAL_IDENTIFIER_EXPANSION='⭐'
